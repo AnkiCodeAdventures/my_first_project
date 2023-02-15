89bff9e50daa9f1bd032947953d52402e4925718
Ankita Agarwal <ankitaag94@gmail.com>
2023-02-15 22:19:26 +0530

    Show homepage

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById("mybutton").addEventListener("click",() => {
+    let person = prompt("Please enter your name", "Harry Potter");
+    if (person) {
+
+        alert(`Hi ${person}, your order coming right up!!!`)
+        alert("Have a nice day!!!")
+        alert("Please visit us again")
+        alert("Also bring your friends")
+    } else {
+        person = prompt("Please enter your name", "Harry Potter");
+    }
+})
