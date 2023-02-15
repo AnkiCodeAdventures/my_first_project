9a284d4f65f230773c3b801ec3ef2f9274147137
Ankita Agarwal <ankitaag94@gmail.com>
2023-02-15 22:35:52 +0530

    Install Prettier

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-document.getElementById("mybutton").addEventListener("click",() => {
-    let person = prompt("Please enter your name", "Harry Potter");
-    if (person) {
-
-        alert(`Hi ${person}, your order coming right up!!!`)
-        alert("Have a nice day!!!")
-        alert("Please visit us again")
-        alert("Also bring your friends")
-    } else {
-        person = prompt("Please enter your name", "Harry Potter");
-    }
-})
+document.getElementById("mybutton").addEventListener("click", () => {
+  let person = prompt("Please enter your name", "Harry Potter");
+  if (person) {
+    alert(`Hi ${person}, your order coming right up!!!`);
+    alert("Have a nice day!!!");
+    alert("Please visit us again");
+    alert("Also bring your friends");
+  } else {
+    person = prompt("Please enter your name", "Harry Potter");
+  }
+});
